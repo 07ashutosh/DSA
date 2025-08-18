921210f95d389218abd5178f8a4a9effb423620b
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-18 14:43:08 +0530

    "bit update"

--- l3_loops.java
+++ l3_loops.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class l3_loops {
-    public static void main(String[] args){
-        //for loop example
+    public static void main(String[] args) {
+        // for loop example
 
         // System.out.println("Enter a number to print its table:");
         // Scanner sc =new Scanner (System.in);
         // int table= sc.nextInt();
         // for(int i=1;i<=10;i++){
-        //     System.out.println(table +"x"+i+"="+(table*i));
+        // System.out.println(table +"x"+i+"="+(table*i));
         // }
 
         // while loop example
@@ -18,10 +18,9 @@ public class l3_loops {
         // int table = sc.nextInt();
         // int i=1;
         // while(i<=10){
-        //     System.out.println(table+"x"+i+"="+(table*i));
-        //     i++;
+        // System.out.println(table+"x"+i+"="+(table*i));
+        // i++;
         // }
-
 
         // do-while loop example/
 
@@ -30,23 +29,23 @@ public class l3_loops {
         // int table = sc.nextInt();
         // int i=1;
         // do{
-        //     System.out.println(table + "x" + i +"=" + (table*i));
-        //     i++;
+        // System.out.println(table + "x" + i +"=" + (table*i));
+        // i++;
         // }while(i<=10);
-
 
         // question solving using loops
 
         // for loop
-        //Qusestion: print the sum of n natural numbers
+        // Qusestion: print the sum of n natural numbers
 
-        // System.out.println("Enter a number to find the sum of first n natural numbers:");
+        // System.out.println("Enter a number to find the sum of first n natural
+        // numbers:");
         // Scanner sc = new Scanner (System.in);
         // int n = sc.nextInt();
         // int sum =0;
 
         // for (int i=1;i<=n;i++){
-        //     sum = sum +i;
+        // sum = sum +i;
         // }
         // System.out.println("sum of the number is :"+sum);
 
@@ -59,31 +58,43 @@ public class l3_loops {
         // using for loop
 
         // for(int i=1;i<=n;i++){
-        //     if(i%2==0){
-        //         System.out.println(i);
-        //     }
+        // if(i%2==0){
+        // System.out.println(i);
+        // }
         // }
 
         // using while loop
 
         // int i=1;
         // while(i<=n){
-        //     if(i%2==0){
-        //         System.out.println(i);
-        //     }
-        //     i++;
+        // if(i%2==0){
+        // System.out.println(i);
+        // }
+        // i++;
         // }
 
         // using do-while loop
 
-        int i=1;
-        do{
-            i++;
-            if(i%2==0){
-                System.out.println(i);
-            }
-        }while(i<=n);
-    
+        // int i=1;
+        // do{
+        // i++;
+        // if(i%2==0){
+        // System.out.println(i);
+        // }
+        // }while(i<=n);
+
+        /*
+          Make a menu driven program. The user can enter 2 numbers, either 1 or 0.
+          If the user enters 1 then keep taking input from the user for a student’s
+          marks(out of 100).
+          If they enter 0 then stop.
+          If he/ she scores :
+          Marks >=90 -> print “This is Good”
+          89 >= Marks >= 60 -> print “This is also Good”
+          59 >= Marks >= 0 -> print “This is Good as well”
+          Because marks don’t matter but our effort does.
+         */
+
     }
-    
+
 }
