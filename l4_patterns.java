565905a66a4bdf530190a02795c305790e80820a
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-19 22:21:35 +0530

    solving pattern problems

--- l4_patterns.java
+++ l4_patterns.java
@@ -12,13 +12,43 @@ public class l4_patterns {
 
         // printing inverted right angle triangle
 
-        for(int i=1;i<=4;i++){
-            for (int j=1;j<=5-i;j++){
+        // for(int i=1;i<=4;i++){
+        //     for (int j=1;j<=5-i;j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+            
+        // }
+
+        // print a hollow rectangle star pattern
+
+        // for(int i=1;i<=4;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(i==1 || i==4 || j==1 || j==5){
+        //             System.out.print(" * ");
+        //         }else{
+        //             System.out.print("   ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        
+        //right angle triangle star pattern
+
+        // for(int i=1;i<=4;i++){
+        //     for (int j=1;j<=i;j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        // printing inverted right angle triangle(method 2)
+
+        for(int i=4;i>=1;i--){
+            for(int j=1;j<=i;j++){
                 System.out.print(" * ");
             }
             System.out.println();
-            
         }
-        
     }
 }
