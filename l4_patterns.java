36743cb3338c43659f2cee994d72b7d8451d7886
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-20 22:05:57 +0530

    printing number patterns

--- l4_patterns.java
+++ l4_patterns.java
@@ -53,15 +53,67 @@ public class l4_patterns {
 
         // printing right side right angle triangle
 
-            for(int i=1;i<=4;i++){
-                for(int j=1;j<=4-i;j++){
-                    System.out.print("   ");
-                }
-                for(int j=1;j<=i;j++){
-                    System.out.print(" * ");
-                }
-                System.out.println();
-            }
+            // for(int i=1;i<=4;i++){
+            //     for(int j=1;j<=4-i;j++){
+            //         System.out.print("   ");
+            //     }
+            //     for(int j=1;j<=i;j++){
+            //         System.out.print(" * ");
+            //     }
+            //     System.out.println();
+            // }
+
+         // printing number right angle triangle
+
+        //  for (int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        //  }
             
+        // reverse right angle triangle
+
+        // for(int i=5;i>=1;i--){
+        //     for (int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // } 
+        // method two
+
+        // for (int i=1;i<=5;i++){
+        //     for(int j=1;j<=5-i+1;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // floyds triangle patern 
+        // int n = 5;
+        // int num = 1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(num+" ");
+        //         num++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // 0-1 triangle pattern
+        
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+
+
     }
 }
