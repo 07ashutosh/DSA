114cc2a03b445267552df30c3b12c78bee7cf0cf
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-20 23:09:57 +0530

    printing star  patterns

--- l4_patterns.java
+++ l4_patterns.java
@@ -102,17 +102,47 @@ public class l4_patterns {
 
         // 0-1 triangle pattern
         
-        int n = 5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                if((i+j)%2==0){
-                    System.out.print("1 ");
-                }else{
-                    System.out.print("0 ");
+        // int n = 5;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         if((i+j)%2==0){
+        //             System.out.print("1 ");
+        //         }else{
+        //             System.out.print("0 ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        //Print a solid rhombus.
+
+        // for(int i=1;i<=4;i++){
+        //         for(int j=1;j<=4-i;j++){
+        //             System.out.print("   ");
+        //         }
+        //         for(int j=1;j<=4;j++){
+        //             System.out.print(" * ");
+        //         }
+        //         System.out.println();
+        //     }
+
+        // primid
+
+        for(int i=1;i<=4;i++){
+                for(int j=1;j<=4-i;j++){
+                    System.out.print("   ");
                 }
+                for(int j=1;j<=i;j++){
+                    System.out.print(" * ");
+                }
+                for(int j=1;j<=i-1;j++){
+                    System.out.print(" * ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
+
+
+
 
 
     }
