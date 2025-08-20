bdbc3f9d6869e1ff9275777637c003b4a111897f
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-20 19:28:35 +0530

    printing star patterns

--- l4_patterns.java
+++ l4_patterns.java
@@ -44,11 +44,24 @@ public class l4_patterns {
         
         // printing inverted right angle triangle(method 2)
 
-        for(int i=4;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print(" * ");
+        // for(int i=4;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // printing right side right angle triangle
+
+            for(int i=1;i<=4;i++){
+                for(int j=1;j<=4-i;j++){
+                    System.out.print("   ");
+                }
+                for(int j=1;j<=i;j++){
+                    System.out.print(" * ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
+            
     }
 }
