94f35d259268f2654305795764dd4eeb22deaa60
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-22 21:30:10 +0530

    primid pattern

--- l5_advancePattern.java
+++ l5_advancePattern.java
@@ -33,16 +33,29 @@ public class l5_advancePattern {
 
       //solid rhombus pattern
       
-      int n=5;
-      for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            System.out.print("   ");
+    //   int n=5;
+    //   for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         System.out.print("   ");
+    //     }
+    //     for(int j=1;j<=n;j++){
+    //         System.out.print(" * ");
+    //     }
+    //     System.out.println();
+    //   }
+
+    //printing pyramid
+
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
         }
-        for(int j=1;j<=n;j++){
-            System.out.print(" * ");
-        }
-        System.out.println();
-      }
     }
     
 }
