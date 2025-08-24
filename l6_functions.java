12eb45673df3b4a26d582281f2b2272fe50a9b33
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-24 16:50:18 +0530

    learning some more functions

--- l6_functions.java
+++ l6_functions.java
@@ -1,11 +1,15 @@
 import java.util.*;
 
 public class l6_functions {
-    public static int add(int x,int y){
-       int sum=x+y;
-        return sum;
-    }
+    // public static int add(int x,int y){
+    //    int sum=x+y;
+    //     return sum;
+    // }
 
+    public static int product(int a,int b){
+        int prod = a*b;
+        return prod;
+    }
     public static void main(String[] args){
         Scanner sc  = new Scanner(System.in);
         
@@ -15,8 +19,11 @@ public class l6_functions {
         System.out.print("Enter second number :");
         int b = sc.nextInt();
 
-        int sum = add(a,b);
-        System.out.println("The sum is :"+sum);
+        // int sum = add(a,b);
+        // System.out.println("The sum is :"+sum);
+
+        int prod = product(a,b);
+        System.out.println("The product is :"+prod);
         sc.close();
     }
     
