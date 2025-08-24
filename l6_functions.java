d4683cbccd0b531e61bd072cef2851be85d24ac9
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-24 17:10:00 +0530

    factorial of n

--- l6_functions.java
+++ l6_functions.java
@@ -6,24 +6,49 @@ public class l6_functions {
     //     return sum;
     // }
 
-    public static int product(int a,int b){
-        int prod = a*b;
-        return prod;
+    // public static int product(int a,int b){
+    //     int prod = a*b;
+    //     return prod;
+    // }
+
+    // factorial of a number
+
+    public static void factorial(int n){
+        int fact = 1;
+
+        if(n<0){
+            System.out.println("invalid number !");
+            return;
+        }
+
+        for (int i=n;i>=1;i--){
+            fact = fact*i;
+        }
+
+        System.out.println("factorial of n number is : "+ fact);;
+
+        return;
     }
     public static void main(String[] args){
         Scanner sc  = new Scanner(System.in);
         
-        System.out.print("Enter first number :");
-        int a = sc.nextInt();
+        // System.out.print("Enter first number :");
+        // int a = sc.nextInt();
 
-        System.out.print("Enter second number :");
-        int b = sc.nextInt();
+        // System.out.print("Enter second number :");
+        // int b = sc.nextInt();
 
         // int sum = add(a,b);
         // System.out.println("The sum is :"+sum);
 
-        int prod = product(a,b);
-        System.out.println("The product is :"+prod);
+        // int prod = product(a,b);
+        // System.out.println("The product is :"+prod);
+
+        System.out.print("enter the number :");
+        int n = sc.nextInt();
+
+        factorial(n);
+
         sc.close();
     }
     
