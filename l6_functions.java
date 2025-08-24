00d60219d750a6432bebea694056bb0dab590cd8
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-24 16:36:53 +0530

    learning functions

---
+++ l6_functions.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class l6_functions {
+    public static int add(int x,int y){
+       int sum=x+y;
+        return sum;
+    }
+
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        
+        System.out.print("Enter first number :");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number :");
+        int b = sc.nextInt();
+
+        int sum = add(a,b);
+        System.out.println("The sum is :"+sum);
+        sc.close();
+    }
+    
+}
