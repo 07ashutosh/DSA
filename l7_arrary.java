b918c808d576b450f6ddd639e75de18a27c51afe
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-26 23:07:52 +0530

    learning array

---
+++ l7_arrary.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class l7_arrary {
+    public static void main(String[] args) {
+        // syntax = type []arrayName = new type[size];
+        int marks[] = new int[3];
+        marks[0] = 97;
+        marks[1] = 96;
+        marks[2] = 95;
+
+        // System.out.println(marks[1]);
+
+        for (int i=0;i<3;i++){
+            System.out.println(marks[i]);
+
+        }
+    }
+
+}
