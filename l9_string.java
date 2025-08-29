6637654ab265974135b1be9ac7e23e6202209d68
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-29 22:06:51 +0530

    reversing a string

--- l9_string.java
+++ l9_string.java
@@ -33,21 +33,41 @@ public class l9_string {
 
 
         StringBuilder sb = new StringBuilder("Abhishek");
+        // System.out.println(sb);
+
+        // //char at
+        // System.out.println(sb.charAt(0));
+
+        // // set char at index
+        // sb.setCharAt(0,'S');
+        // System.out.println(sb);
+
+        // //insert char at index
+        // sb.insert(0,'A');
+        // System.out.println(sb);
+
+        // //delete char at index
+        // sb.delete(1,2);
+        // System.out.println(sb);
+
+        // apend in the string
+        // sb.append(" tiwari");
+        // System.out.println(sb);
+
+        // reversing a string
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front = i;
+            int back = sb.length()-1-i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front,backChar);
+            sb.setCharAt(back,frontChar);
+
+        }
         System.out.println(sb);
 
-        //char at
-        System.out.println(sb.charAt(0));
-
-        // set char at index
-        sb.setCharAt(0,'S');
-        System.out.println(sb);
-
-        //insert char at index
-        sb.insert(0,'A');
-        System.out.println(sb);
-
-        //delete char at index
-        sb.delete(1,2);
-        System.out.println(sb);
     }
 }
