6a5610d6b02fdcbe879b0846dc40fee8847ef537
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-29 21:17:25 +0530

    string comparision

--- l9_string.java
+++ l9_string.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class l9_string {
     public static void main(String[] args){
-        String name = "Ashutosh";
+        // String name = "Ashutosh";
         // System.out.println(name);
         // System.out.println(name.length());
         // System.out.println(name.toLowerCase());
@@ -8,11 +10,25 @@ public class l9_string {
         // System.out.println(name.substring(0,3));
         // System.out.println(name.replace('r','p'));
         // System.out.println(name.indexOf('r'));
-        System.out.println(name.lastIndexOf('t'));
+        // System.out.println(name.lastIndexOf('t'));
         // System.out.println(name.charAt(4));
         // System.out.println(name.equals("Ashutosh"));
         // System.out.println(name.equalsIgnoreCase("ashutosh"));
         // System.out.println("I am escape sequence \t double quote");
         // System.out.println("I am escape sequence \\ backslash");
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your name :");
+        // String name = sc.nextLine();
+
+        // System.out.println("hello "+name);
+
+        //compare two string 
+        String name1= "ashu";
+        String name2="ashu";
+        System.out.println(name1.compareTo(name2));
+
+
+        // sc.close();
     }
 }
