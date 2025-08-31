f6e34b13e3482ebb1f84e3d92d4d1aaced4d4958
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-08-31 23:21:22 +0530

    solving basic recursion questions

--- l12Recursion.java
+++ l12Recursion.java
@@ -1,19 +1,49 @@
 public class l12Recursion {
 
-    public static void printNumbers(int n){
-        if(n==0){
+    // public static void printNumbers(int n){
+    //     if(n==0){
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     printNumbers(n-1);
+    //     ;
+    // }
+
+
+    // public static void printNumbers(int n){
+    //     if(n==6){
+    //         return;
+    //     }
+    //     System.out.println(n);
+    //     printNumbers(n+1);
+    //     ;
+    // }
+
+    public static void sumNumbers(int i ,int n , int sum){
+        if(i==n){
+            sum +=i;
+            System.out.println(sum);
             return;
         }
-        System.out.println(n);
-        printNumbers(n-1);
-        ;
+        sum +=i;
+        sumNumbers(i+1, n, sum);
+
     }
 
     public static void main(String[] args){
 
         //print numbers from n to 1
-        int n = 5;
-        printNumbers(n);
+        // int n = 5;
+        // printNumbers(n);
+
+        
+        // print number from 1 to 5 
+        // int n = 1;
+        // printNumbers(n);
+
+
+        //Q: print sum of n natural no(s)
+        sumNumbers(1,5,0);
 
     }
 }
