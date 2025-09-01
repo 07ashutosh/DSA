33eb42b3a372bdd1f8b0d2cd0b8c7dee03a56b09
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-01 21:39:27 +0530

    solving factorial calculation using recursion

--- l12Recursion.java
+++ l12Recursion.java
@@ -1,49 +1,59 @@
 public class l12Recursion {
 
     // public static void printNumbers(int n){
-    //     if(n==0){
-    //         return;
-    //     }
-    //     System.out.println(n);
-    //     printNumbers(n-1);
-    //     ;
+    // if(n==0){
+    // return;
     // }
-
+    // System.out.println(n);
+    // printNumbers(n-1);
+    // ;
+    // }
 
     // public static void printNumbers(int n){
-    //     if(n==6){
-    //         return;
-    //     }
-    //     System.out.println(n);
-    //     printNumbers(n+1);
-    //     ;
+    // if(n==6){
+    // return;
+    // }
+    // System.out.println(n);
+    // printNumbers(n+1);
+    // ;
     // }
 
-    public static void sumNumbers(int i ,int n , int sum){
-        if(i==n){
-            sum +=i;
-            System.out.println(sum);
-            return;
-        }
-        sum +=i;
-        sumNumbers(i+1, n, sum);
+    // public static void sumNumbers(int i ,int n , int sum){
+    // if(i==n){
+    // sum +=i;
+    // System.out.println(sum);
+    // return;
+    // }
+    // sum +=i;
+    // sumNumbers(i+1, n, sum);
+    // }
 
+    public static int calcFactorial(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+
+        int nm1 = calcFactorial(n - 1);
+        int fact = n * nm1;
+        return fact;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //print numbers from n to 1
+        // print numbers from n to 1
         // int n = 5;
         // printNumbers(n);
 
-        
-        // print number from 1 to 5 
+        // print number from 1 to 5
         // int n = 1;
         // printNumbers(n);
 
+        // Q: print sum of n natural no(s)
+        // sumNumbers(1,5,0);
 
-        //Q: print sum of n natural no(s)
-        sumNumbers(1,5,0);
+        // Q: calculating factorial of n
+        int n=5;
+        System.out.println(calcFactorial(n));
 
     }
 }
