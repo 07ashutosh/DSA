389bd8457521effcc30c51435c6923833ffff095
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-11 21:40:35 +0530

    oops concept, inheritence

--- l17_oops.java
+++ l17_oops.java
@@ -1,49 +1,84 @@
-class Student{
-    String name;
-    int age;
+// class Student{
+//     String name;
+//     int age;
 
-    // public void printinfo(){
-    //     System.out.println(this.name);
-    //     System.out.println(this.age);
-    // }
+//     // public void printinfo(){
+//     //     System.out.println(this.name);
+//     //     System.out.println(this.age);
+//     // }
 
-    //non peramertized constructor
+//     //non peramertized constructor
 
-    // Student(){
-    //     System.out.println("constructoe is called");
-    // }
-    //perameterized constructor
+//     // Student(){
+//     //     System.out.println("constructoe is called");
+//     // }
+//     //perameterized constructor
 
-    // Student(String name, int age){
+//     // Student(String name, int age){
     
-    //     this.name = name;
-    //     this.age = age;
-    // }
+//     //     this.name = name;
+//     //     this.age = age;
+//     // }
 
-    //polimorfismo
-    public void printinfo(String name){
-        System.out.println(name);
-    }
+//     //polimorfismo
+//     public void printinfo(String name){
+//         System.out.println(name);
+//     }
 
-    public void printinfo(int age){
-        System.out.println(age);
-    }
+//     public void printinfo(int age){
+//         System.out.println(age);
+//     }
 
-    public void printinfo(String name,int age){
-        System.out.println(name + " " + age);
-    }
+//     public void printinfo(String name,int age){
+//         System.out.println(name + " " + age);
+//     }
 
-}
-public class l17_oops {
-    public static void main(String[] args){
-        Student stud1 = new Student();
-        stud1.name = "nancy";
-        stud1.age = 22;
+// }
+// public class l17_oops {
+//     public static void main(String[] args){
+//         Student stud1 = new Student();
+//         stud1.name = "nancy";
+//         stud1.age = 22;
         
-        stud1.printinfo(stud1.name);
-        stud1.printinfo(stud1.age);
-        stud1.printinfo(stud1.name,stud1.age);
+//         stud1.printinfo(stud1.name);
+//         stud1.printinfo(stud1.age);
+//         stud1.printinfo(stud1.name,stud1.age);
         
-    }
+//     }
     
+// }
+
+// inheritance
+
+public class l17_oops{
+    // single level inheritance
+    class Shapes{
+        public void area(){
+            System.out.println("print area");
+        }
+    }
+    class Triangle extends Shapes{
+        public void area(int l, int h){
+            System.out.println(1/2*l*h);
+        }
+    }
+
+    //start of multilevel inheritance
+    class EquilateralTriangle extends Triangle{
+        public void area(int l, int h){
+            System.out.println(1/2*l*h);
+        }
+    }
+
+    //hierarchial inhritence (when a single parent class is inherit by multiple child)
+    class Circle extends Shapes{
+        public void area(int r){
+            System.out.println((3.14)*r*r);
+        }
+    }
+    public static void main(String [] args){
+
+    }
 }
+
+
