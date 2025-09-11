8073a28c900c83b28dd962e166db385e4e14255f
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-11 20:13:31 +0530

    oops concept,polimorfisom overloading

--- l17_oops.java
+++ l17_oops.java
@@ -2,28 +2,48 @@ class Student{
     String name;
     int age;
 
-    public void printinfo(){
-        System.out.println(this.name);
-        System.out.println(this.age);
-    }
+    // public void printinfo(){
+    //     System.out.println(this.name);
+    //     System.out.println(this.age);
+    // }
 
     //non peramertized constructor
 
     // Student(){
     //     System.out.println("constructoe is called");
     // }
-
     //perameterized constructor
 
-    Student(String name, int age){
-        this.name = name;
-        this.age = age;
+    // Student(String name, int age){
+    
+    //     this.name = name;
+    //     this.age = age;
+    // }
+
+    //polimorfismo
+    public void printinfo(String name){
+        System.out.println(name);
     }
+
+    public void printinfo(int age){
+        System.out.println(age);
+    }
+
+    public void printinfo(String name,int age){
+        System.out.println(name + " " + age);
+    }
+
 }
 public class l17_oops {
     public static void main(String[] args){
-        Student stud1 = new Student("bhawna",21);
-        stud1.printinfo();
+        Student stud1 = new Student();
+        stud1.name = "nancy";
+        stud1.age = 22;
+        
+        stud1.printinfo(stud1.name);
+        stud1.printinfo(stud1.age);
+        stud1.printinfo(stud1.name,stud1.age);
+        
     }
     
 }
