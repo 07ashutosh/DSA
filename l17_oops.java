b54610a479199b6f8012e31c7532f324f698bb47
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-11 19:25:02 +0530

    oops concept,constructor

--- l17_oops.java
+++ l17_oops.java
@@ -6,14 +6,23 @@ class Student{
         System.out.println(this.name);
         System.out.println(this.age);
     }
+
+    //non peramertized constructor
+
+    // Student(){
+    //     System.out.println("constructoe is called");
+    // }
+
+    //perameterized constructor
+
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 }
 public class l17_oops {
     public static void main(String[] args){
-        Student stud1 = new Student();
-
-        stud1.name="ashutosh";
-        stud1.age = 22;
-
+        Student stud1 = new Student("bhawna",21);
         stud1.printinfo();
     }
     
